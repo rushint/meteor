4d7665bd3ee4a8ca9102156a69c49e76ed82aabf
Scott Patten <scott@scottpatten.ca>
2012-09-24 15:48:27 -0700

    Cleanup on Meteor.RemoteCollectionDriverWithCallbacks -- moving arguments around so that changes to the model in the before filter get kept, Making sure only one connection to Mongo is ever made and avoiding the horrible monkeypatching of String.capitalize.

--- packages/callbacks/remote_collection_driver_with_callbacks.js
+++ packages/callbacks/remote_collection_driver_with_callbacks.js
@@ -1,16 +1,16 @@
-String.prototype.capitalize = function() { return this.charAt(0).toUpperCase() + this.slice(1);}
+// XXX: Use _.str.capitalize instead of shoving this into the global namespace
+capitalizeString = function(str) { return str.charAt(0).toUpperCase() + str.slice(1);}
 
 Meteor.RemoteCollectionDriverWithCallbacks = function (name) {
   var self = this;
   self.name = name;
-  self.mongo_url = process.env.MONGO_URL;
-  self.mongo = new Meteor._Mongo(self.mongo_url);
+  self.mongo = Meteor.RemoteCollectionDriverWithCallbacks.connection;
   self.actions = Meteor.RemoteCollectionDriverWithCallbacks.actions;
   self.timings = Meteor.RemoteCollectionDriverWithCallbacks.timings;
   self.callbacks = {};
   _.each(self.actions, function(action) {
     _.each(self.timings, function(timing) {
-      callbackName = timing + action.capitalize()
+      callbackName = timing + capitalizeString(action)
       self.callbacks[callbackName] = []});
   });
 };
@@ -18,20 +18,24 @@ Meteor.RemoteCollectionDriverWithCallbacks = function (name) {
 Meteor.RemoteCollectionDriverWithCallbacks.actions = ['find', 'findOne', 'insert', 'update', 'remove'];
 Meteor.RemoteCollectionDriverWithCallbacks.timings = ['before', 'after'];
 
+Meteor.RemoteCollectionDriverWithCallbacks.connection = new Meteor._Mongo(process.env.MONGO_URL);
+
 _.each(Meteor.RemoteCollectionDriverWithCallbacks.actions, function(action) {
   Meteor.RemoteCollectionDriverWithCallbacks.prototype[action] = function() {
     // Turn arguments into an array so we can unshift this.name onto it
     // before sending it to this.mongo.insert (or find or findOne or whatever)
-    var args = arguments;
-    var argsForMongo = Array.prototype.slice.call(arguments);
-    argsForMongo.unshift(this.name)
-    var befores = this.callbacks["before" + action.capitalize()]
-    var afters = this.callbacks["after" + action.capitalize()]
+    var args = Array.prototype.slice.call(arguments);
+    var befores = this.callbacks["before" + capitalizeString(action)]
 
     _.each(befores, function(before) {
       before.apply(this, args)
     })
+
+    var argsForMongo = [this.name].concat(args)
     res =  this.mongo[action].apply(this.mongo, argsForMongo)
+
+    var afters = this.callbacks["after" + capitalizeString(action)]
+
     _.each(afters, function(after) {
       after.apply(this, args)
     })
@@ -39,7 +43,7 @@ _.each(Meteor.RemoteCollectionDriverWithCallbacks.actions, function(action) {
   }
 
   _.each(Meteor.RemoteCollectionDriverWithCallbacks.timings, function(timing) {
-    var callbackName = timing + action.capitalize()
+    var callbackName = timing + capitalizeString(action)
     Meteor.RemoteCollectionDriverWithCallbacks.prototype[callbackName] = function(fn) {
       this.callbacks[callbackName].push(fn)
     }
